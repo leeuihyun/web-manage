324bed49ba73eba323aebaf450262ab4cb480ad2
leeuihyun <meta212121@naver.com>
2022-01-23 19:57:34 +0900

    Web Manager-sys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 import Customer from "./components/Customer";
 import "./App.css";
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from "@material-ui/styles";
+import { Paper } from "@material-ui/core";
+
+
+const styles = ({
+  root : {
+    width : '100%',
+    overflowX : "auto"
+  },
+  table : {
+    minWidth : 1080
+  }
+})
 
 const customers = [
   {
@@ -30,9 +48,22 @@ const customers = [
 ];
 class App extends Component {
   render() {
+    const {classes} = this.props ? this.props:null;
     return (
-      <div>
-        {customers.map((c) => {
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {customers.map((c) => {
           return (
             <Customer
               key = {c.id} 
@@ -45,9 +76,12 @@ class App extends Component {
             ></Customer>
           );
         })}
-      </div>
+          </TableBody>
+        </Table>
+        
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 export default class Customer extends Component {
   render() {
     return (
-        <div>
-            <CustomerProfile image = {this.props.image} id = {this.props.id} name = {this.props.name}></CustomerProfile>
-            <CustomerInfo birthday = {this.props.birthday} gender = {this.props.gender} job = {this.props.job}></CustomerInfo>
-
-        </div>
+        <TableRow>
+            <TableCell>{this.props.id}</TableCell>
+            <TableCell><img src={this.props.image} alt="Profile" /></TableCell>
+            <TableCell>{this.props.name}</TableCell>
+            <TableCell>{this.props.birthday}</TableCell>
+            <TableCell>{this.props.gender}</TableCell>
+            <TableCell>{this.props.job}</TableCell>
+        </TableRow>
     );
   }
 }
 
-class CustomerProfile extends Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="Profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
